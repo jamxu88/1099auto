49edc357e780484874b7fa89e998c24571993cb9
James <james@jamesxu.dev>
2024-01-03 15:46:44 -0500

    Commit

---
+++ 1099.py
@@ -0,0 +1,67 @@
+import pyautogui
+import csv
+import time
+pyautogui.PAUSE = 0.01
+with open('FILENAME.csv') as file:
+    time.sleep(10)
+    csv_reader = csv.reader(file, delimiter=',')
+    reader = csv.DictReader(file)
+
+    # Loop through each row
+    for row in reader:
+        tin = row["TIN"]
+        amount = row["Amount"]
+        first_name = row["First Name"]
+        last_name = row["Last Name"]
+        middle_initial = row["M.I."]
+        business_name = row["Business Name"]
+        address_line_1 = row["Address Line 1"]
+        address_line_2 = row["Address Line 2"]
+        zip = row["Zip"]
+
+        # Print out the values in the row
+        print(tin, amount, first_name, last_name, middle_initial, business_name, address_line_1, address_line_2, zip)
+        pyautogui.write(f'{tin}')  ## SSN
+        pyautogui.press('tab')
+        time.sleep(1)
+        pyautogui.write(f'{first_name}') ## FNAME
+        pyautogui.press('tab')
+        time.sleep(1)
+        if middle_initial != "":
+            pyautogui.write(f'{middle_initial}') ## MNAME
+        pyautogui.press('tab') 
+        pyautogui.write(f'{last_name}') ## LNAME
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        time.sleep(1)
+        if business_name != "":
+            pyautogui.write(f'{business_name}') ## Business
+        pyautogui.press('tab') 
+        pyautogui.write(f'{address_line_1}') ## Street ln 1
+        pyautogui.press('tab')
+        time.sleep(1)
+        if address_line_2 != "":
+            pyautogui.write(f'{address_line_2}') ## Street ln2
+        pyautogui.press('tab') 
+        pyautogui.write(f'{zip}') ## ZIP
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'c')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.write(f'{amount}')
+        time.sleep(1)
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.write(f'{amount}')
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(1)
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 1099auto
 Automatically fill out ATXW2 forms.
+
+## Preparing the CSV file
+1. Ensure that the Excel file has the following columns:
+- `TIN`
+- `Amount` (Compensation/Income)
+- `First Name`
+- `Last Name`
+- `M.I.`
+- `Business Name`
+- `Address Line 1`
+- `Address Line 2`
+- `Zip`
+
+2. Save the excel file as a CSV file.
+
+## Usage
+Install python and dependencies in `requirements.txt`.
+
+In `1099.py`, replace `FILENAME.csv` in line 5 with the name of the CSV file you saved.
+
+Run `python 1099.py` and switch to the ATX W-2 program. Click on the RECIPIENT'S TIN field. The program will automatically fill out the forms. There will be a 10 second delay between running the program and the form being filled so you can navigate to the field in time.
